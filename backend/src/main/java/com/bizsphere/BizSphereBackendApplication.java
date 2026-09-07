48745e196ab304c9cdffccf6e189bd8228417390
Burhanuddin Saifee <burhanuddinsaifee1852@gmail.com>
2026-09-07 23:40:36 +0530

    Initial BizSphere starter setup

---
+++ backend/src/main/java/com/bizsphere/BizSphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bizsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizSphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizSphereBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bizsphere/BizSphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizSphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
